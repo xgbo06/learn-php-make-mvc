6d119cd8f4b8c0f6a97b6949b0f176ecd73acb3f
xgbo06 <xgbo006@gmail.com>
2019-05-11 23:24:33 +0530

    subdirectory namespaces

--- public/index.php
+++ public/index.php
@@ -21,6 +21,7 @@
 	$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 	$router->add('{controller}/{action}');
 	$router->add('{controller}/{id:\d+}/{action}');
+	$router->add('admin/{controller}/{action}', ['namespace' => 'Admin']);
 	
 /*
 	$url = $_SERVER['QUERY_STRING'];
