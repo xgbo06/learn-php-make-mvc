91334a6d14dc1cf2019dd9c767124ed6cacc716a
xgbo06 <xgbo006@gmail.com>
2019-05-18 23:13:30 +0530

    singel db connection for model

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use PDO;
 
-class Post
+class Post extends \Core\Model
 {
 
     /**
@@ -14,14 +14,16 @@ class Post
      */
     public static function getAll()
     {
-        $host = 'localhost';
-        $dbname = 'mvc';
-        $username = 'root';
-        $password = '';
+        // $host = 'localhost';
+        // $dbname = 'mvc';
+        // $username = 'root';
+        // $password = '';
     
         try {
-            $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",
-                          $username, $password);
+            // $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",
+            //               $username, $password);
+
+            $db = static::getDB();
 
             $stmt = $db->query('SELECT id, title, content FROM posts
                                 ORDER BY created_at');
